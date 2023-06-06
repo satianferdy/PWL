0eb69d2a40cb70288b0a209bb278c5636231b99a
satianferdy <satianferdy@gmail.com>
2023-06-06 19:11:23 +0700

    starter code

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ContactUs;
+// use App\Http\Controllers\ContactUs;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,31 +14,9 @@ use App\Http\Controllers\ContactUs;
 |
 */
 
-Route::prefix('/')->group(function () {
-    Route::get('/', function () {
-        return view('master.home');
-    })->name('home');
-
-    Route::get('/product', function () {
-        return view('master.product');
-    })->name('product');
-
-    Route::get('/program', function () {
-        return view('master.program');
-    })->name('program');
+Route::get('/', function () {
+    // return view('welcome');
+    return view('master.home');
 });
-
-Route::get('/news/{id}', function ($id) {
-    return view('master.news', ['id' => $id]);
-});
-
-Route::get('/about-us', function () {
-    return view('master.about-us');
-})->name('about-us');
-
-
-Route::resource('/contact-us', ContactUs::class)->only(['index']);
-// Route::resource('about-us', 'aboutUs');
-// Route::resource('contact-us', 'ContactUs');
 
 
